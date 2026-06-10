042fdef0bbcb0fd2bf529d6a55498b9b91ec2713
Dhanalakshmi D <dhanalakshmidgowd04@gmail.com>
2026-06-10 14:41:06 +0530

    01-folder

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const APP = (_ => {
+    return (
+        <>
+        <div id='A'>
+            <h1>React App</h1>
+            <h2>Learning it</h2>
+  
+  
+  
+        </div>
+        <div id= 'B'>hiii</div>
+        
+        
+        </>
+    )
+}
+    
+);
+export default APP;
