20c03a2a761134c9062cb0e3b54859510cf97a52
Dhanalakshmi D <dhanalakshmidgowd04@gmail.com>
2026-06-11 12:53:15 +0530

    UI project complete

---
+++ 07-UI-Project/src/components/Section2/Section2.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const Section2 = () => {
+    return (
+        <div className = 'h-screen bg-mist-400'>
+            Section 2
+        </div>
+    )
+}
+export default Section2;
